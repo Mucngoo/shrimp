bc6b873a839f6f62615d7ef8c524f2e4a949aa31
Mucngoo <s4029319@rmit.edu.vn>
2025-11-17 22:30:04 +0700

    aaa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import ProductCard from './components/ProductCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
+        <ProductCard status="New" name="Tesla Model 3 Standard Range Plus" price="360.000.000 đ" location="Florida, USA" year={2020} wheel="Rear-wheel Drive" fuel="electric" seats={5}/>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </div>
   );
 }
