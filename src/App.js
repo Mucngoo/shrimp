ab47eeb45c8ea1be892e26ef0eb6979ae940301c
Mucngoo <s4029319@rmit.edu.vn>
2025-11-17 23:33:00 +0700

    vavd

--- src/App.js
+++ src/App.js
@@ -4,12 +4,43 @@ import ProductCard from './components/ProductCard';
 function App() {
   return (
     <div className="App">
-      <div className="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
-        <ProductCard status="New" name="Tesla Model 3 Standard Range Plus" price="360.000.000 đ" location="Florida, USA" year={2020} wheel="Rear-wheel Drive" fuel="electric" seats={5}/>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+      <div className="flex gap-8">
+        <ProductCard img="https://photo.znews.vn/w660/Uploaded/bpivptvl/2025_07_07/tesla_models_caranddriver.jpg" status="New" name="Tesla Model 3 Standard Range Plus" price="360.000.000 đ" location="Florida, USA" year={2020} wheel="Rear-wheel Drive" fuel="Electric" seats={5}/>
+        <ProductCard 
+          img="https://bmw-hanoi.com.vn/wp-content/uploads/BMW-840i-Gran-Coupe-BMW-Hanoi.com_.vn10-1.jpg" 
+          status="Used" 
+          name="BMW i4 M50" 
+          price="1.200.000.000 đ" 
+          location="California, USA" 
+          year={2022} 
+          wheel="All-wheel Drive" 
+          fuel="Electric" 
+          seats={5} 
+        />
+
+        <ProductCard 
+          img="https://www.topgear.com/sites/default/files/cars-car/image/2025/05/Original-49014-mercedes-e53-amg-saloon-0002.jpg" 
+          status="Used" 
+          name="Mercedes-Benz C-Class 300" 
+          price="900.000.000 đ" 
+          location="New York, USA" 
+          year={2021} 
+          wheel="Rear-wheel Drive" 
+          fuel="Gasoline" 
+          seats={5} 
+        />
+
+        <ProductCard 
+          img="https://img1.oto.com.vn/2024/12/17/OpzfnMD2/audi-a6-gia-xe-058f.webp" 
+          status="New" 
+          name="Audi e-tron GT" 
+          price="3.500.000.000 đ" 
+          location="Florida, USA" 
+          year={2023} 
+          wheel="All-wheel Drive" 
+          fuel="Electric" 
+          seats={5} 
+        />
       </div>
     </div>
   );
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,69 +1,71 @@
 function ProductCard(props) {
     return (
-        <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
-            <div class="h-56 w-full">
+        <div className="min-w-[440px] max-w-[440px] max-h-[600px] rounded-lg border border-blue-300 bg-white p-6 shadow-sm dark:border-blue-300 dark:bg-gray-900">
+            <div className="h-56 w-full">
             <a href="#">
-                <img class="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front.svg" alt="" />
-                <img class="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/imac-front-dark.svg" alt="" />
+                <img className="mx-auto h-full dark:hidden" src={props.img} alt="" />
+                <img className="mx-auto hidden h-full dark:block" src={props.img} alt="" />
             </a>
             </div>
-            <div class="pt-6">
-            <div class="mb-4 flex items-center justify-between gap-4">
-                <div className="border border-blue-400 text-blue-400 px-2 py-1 rounded-lg bg-transparent">
+            <div className="pt-6">
+            <div className="mb-4 flex items-center justify-between gap-4">
+                <div className="border border-blue-500 text-blue-500 px-3 rounded-lg bg-transparent">
                     {props.status}
                 </div>
 
-                <div class="flex items-center justify-end gap-1">
-                    <button type="button" data-tooltip-target="tooltip-add-to-favorites" class="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                        <span class="sr-only"> Add to Favorites </span>
-                        <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
-                        </svg>
-                    </button>
-                    <div id="tooltip-add-to-favorites" role="tooltip" class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700" data-popper-placement="top">
-                        Add to favorites
-                        <div class="tooltip-arrow" data-popper-arrow=""></div>
+                <div className="flex items-center justify-end gap-1">
+                    <div className="relative flex items-center justify-end gap-1 group">
+                        <button type="button" className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                            <span className="sr-only">Add to Favorites</span>
+                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z" />
+                            </svg>
+                        </button>
+
+                        <div className="absolute bottom-full mb-2 hidden w-max rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white group-hover:block dark:bg-gray-700">
+                            Add to favorites
+                        </div>
                     </div>
                 </div>
             </div>
 
-            <a href="#" class="text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{props.name}</a>
+            <a href="#" className="text-2xl font-semibold leading-tight text-gray-900 hover:underline dark:text-white">{props.name}</a>
 
-            <div class="mt-2 flex items-center gap-2">
-                <p class="text-sm font-medium text-gray-900 dark:text-white">{props.price}</p>
-                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{props.location}</p>
+            <div className="mt-3 flex flex-col gap-2">
+                <p className="text-2xl font-medium text-blue-500">{props.price}</p>
+                <p className="text-lg text-gray-900 dark:text-white">{props.location}</p>
             </div>
 
-            <ul class="mt-2 flex items-center gap-4">
-                <li class="flex items-center gap-2">
-                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <ul className="mt-1 flex items-center gap-4">
+                <li className="flex items-center gap-2">
+                <svg className="h-4 w-4 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"/>
                 </svg>
-                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{props.year}</p>
+                <p className="text-lg text-gray-900 dark:text-white">{props.year}</p>
                 </li>
 
-                <li class="flex items-center gap-2">
-                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+                <li className="flex items-center gap-2">
+                <svg className="h-5 w-5 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"/>
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
                 </svg>
-                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{props.wheel}</p>
+                <p className="text-lg text-gray-900 dark:text-white">{props.wheel}</p>
                 </li>
             </ul>
 
-            <ul class="mt-2 flex items-center gap-4">
-                <li class="flex items-center gap-2">
-                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.287 5H15m3.852 3H15m3.384 3H15m-9.47-.764h-.972A14.755 14.755 0 0 1 4.445 8c-.019-.747.019-1.494.113-2.236h.972a.95.95 0 0 0 .946-.856l.188-1.877a.951.951 0 0 0-.946-1.046H3.791a1.127 1.127 0 0 0-1.067.75A16.11 16.11 0 0 0 2 8a16.737 16.737 0 0 0 .743 5.242c.154.463 1.255.773 1.743.773h1.232a.95.95 0 0 0 .946-1.046l-.188-1.877a.95.95 0 0 0-.946-.856ZM19.063 2H10v12h8.273l1.716-10.847A.981.981 0 0 0 19.063 2ZM20 18H9v-2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v2Z"/>
+            <ul className="mt-2 flex items-center gap-4">
+                <li className="flex items-center gap-2">
+                <svg className="h-4 w-4 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19.287 5H15m3.852 3H15m3.384 3H15m-9.47-.764h-.972A14.755 14.755 0 0 1 4.445 8c-.019-.747.019-1.494.113-2.236h.972a.95.95 0 0 0 .946-.856l.188-1.877a.951.951 0 0 0-.946-1.046H3.791a1.127 1.127 0 0 0-1.067.75A16.11 16.11 0 0 0 2 8a16.737 16.737 0 0 0 .743 5.242c.154.463 1.255.773 1.743.773h1.232a.95.95 0 0 0 .946-1.046l-.188-1.877a.95.95 0 0 0-.946-.856ZM19.063 2H10v12h8.273l1.716-10.847A.981.981 0 0 0 19.063 2ZM20 18H9v-2a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v2Z"/>
                 </svg>
-                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{props.fuel}</p>
+                <p className="text-lg text-gray-900 dark:text-white">{props.fuel}</p>
                 </li>
 
-                <li class="flex items-center gap-2">
-                <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                <li className="flex items-center gap-2">
+                <svg className="h-5 w-5 text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 22">
+                    <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M16 19h4a1 1 0 0 0 1-1v-1a3 3 0 0 0-3-3h-2m-2.236-4a3 3 0 1 0 0-4M3 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
                 </svg>
-                <p class="text-sm font-medium text-gray-500 dark:text-gray-400">{props.seats}</p>
+                <p className="text-lg text-gray-900 dark:text-white">{props.seats}</p>
                 </li>
             </ul>
             </div>
